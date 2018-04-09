dea8bd70302c9c18ba04cfeb3f906e73aa194c79
Donald Blanc <dxb3662@rit.edu>
2018-04-09 09:16:41 -0400

    Added infinite scroll section for the commics

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -15,7 +15,7 @@ var queryURL = marvURL + charSearch + name + apiAuth;
 
 console.log(queryURL);
 
-var test = " https://gateway.marvel.com/v1/public/characters?name=spider-man&ts=1&apikey=0c59a3c44e014ddc1ffcdf3ff74fc90f&hash=a6c84b3cc66e3a92494635469be8bd74";
+//var test = " https://gateway.marvel.com/v1/public/characters?name=spider-man&ts=1&apikey=0c59a3c44e014ddc1ffcdf3ff74fc90f&hash=a6c84b3cc66e3a92494635469be8bd74";
 
 // function to get my ID
 var getTheId = function (idname) {
@@ -41,7 +41,6 @@ searchButton.onclick = function (event) {
   } else {
     alert("you have to enter a character");
   }
-
 
 };
 
@@ -82,21 +81,23 @@ function getQuery(search) {
       charId = response.data.results[0].id;
 
       printToPage(response);
-
+      return response;
     });
 
 }//getQuery
 
 
 function getComics(charId) {
-
-  let newURL = marvURL + "characters/" + charId + "/comics?limit=100" + apiAuth;
+// need to set a boolean here for if a user has chosen a new character.
+//  let newURL = marvURL + "characters/" + charId + "/comics?limit=100&offset=100" + apiAuth;
+  let newURL = marvURL + "characters/" + charId + "/comics?limit=100&offset=100" + apiAuth;
   console.log("The comics fired");
   console.log(charId);
   console.log(newURL);
-  getQuery(newURL);
-  //https://gateway.marvel.com:443/v1/public/characters/1009624/comics?limit=100&apikey=0c59a3c44e014ddc1ffcdf3ff74fc90f
+  loadMore(charId);
+    
 
+  //https://gateway.marvel.com:443/v1/public/characters/1009624/comics?limit=100&apikey=0c59a3c44e014ddc1ffcdf3ff74fc90f
 
 }//getComics 
 
@@ -126,9 +127,65 @@ function printToPage(response) {
   comicDiv.append(charImage);
   comicDiv.append(p);
 
-
   $("#comics").append(comicDiv);
 
-
-
 }//printToPage
+
+
+
+// infinite scroll section for comic Div
+var listElm = document.querySelector('#infinite-comiclist');
+// Add 20 items.
+var nextItem = 0;
+//var loadMore = function () 
+function loadMore(char){
+
+  let charId = "1009610";
+
+  let newURL = marvURL + "characters/" + charId + "/comics?limit=10&offset=" +nextItem + apiAuth;
+  const kevsServer = "https://mighty-river-19291.herokuapp.com/cors";
+  var data = {
+    url: newURL,
+    key: "8b5dcaf7cdfb9c46221d492eec6560c571d6ec218b2485c54075ab7840fa77f9"
+  }
+
+  fetch(kevsServer, {
+    method: 'POST',
+    body: JSON.stringify(data),
+    headers: new Headers({
+      'Content-Type': 'application/json'
+    })
+
+  }).then(response => response.json())
+    .catch(error => console.error('Error:', error))
+    .then(function (response) {
+      console.log("****THE Load more function****");
+      console.log(response);
+   // console.log(Object.keys(response).length);
+    console.log(response.data.results.length);
+     // console.log(response.data.results[0].title);
+
+      // debuggin lines printing title to console.
+      for (var i = 0; i < response.data.results.length ; i++) {
+        console.log(response.data.results[i].title);
+      }
+      
+      for (var i = 0; i < response.data.results.length ; i++) {
+         var item = document.createElement('li');
+         item.innerText = 'Title ' + response.data.results[i].title;
+         listElm.appendChild(item);
+         console.log("Comic Image: " +response.data.results[i].thumbnail.path + "." + response.data.results[i].thumbnail.extension);
+       }
+
+       nextItem += 11;
+       console.log("The Value of nextItem :" +  nextItem );    
+    });
+    
+}
+
+// Detect when scrolled to bottom.
+listElm.addEventListener('scroll', function () {
+  if (listElm.scrollTop + listElm.clientHeight >= listElm.scrollHeight) {
+    loadMore();
+  }
+});
