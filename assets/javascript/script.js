ff9304233088529f2655f89a02aa01f1e92a573f
Jose Viteri <Jose@Joses-MBP.home>
2018-04-09 23:58:12 -0400

    updated array

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,164 +1,203 @@
 
 // Jose's CODE 
 const movies = ["Howard the Duck", "The Punisher ", "Captain America", "The Fantastic Four (1994)", "Blade",
-    "X-Men",
-    "Blade II",
-    "Spider-Man",
-    "Daredevil",
-    "Hulk",
-    "Spider-Man 2",
-    "Blade: Trinity",
-    "Elektra",
-    "Man-Thing",
-    "Fantastic Four",
-    "X-Men: The Last Stand",
-    "Ghost Rider",
-    "Spider-Man 3",
-    "Fantastic Four: Rise of the Silver Surfer",
-    "Iron Man",
-    "The Incredible Hulk",
-    "Punisher: War Zone",
-    "X-Men Origins",
-    "Iron Man 2",
-    "Thor",
-    "X-Men: First Class",
-    "Captain America: The First Avenger",
-    "Ghost Rider: Spirit of Vengeance",
-    "Avengers",
-    "The Amazing Spider-Man",
-    "Iron Man 3",
-    "The Wolverine",
-    "Thor: The Dark World",
-    "Captain America: The Winter Soldier",
-    "The Amazing Spider-Man 2",
-    "X-Men: Days of Future Past",
-    "Guardians of the Galaxy",
-    "Big Hero 6",
-    "Avengers: Age of Ultron",
-    "Ant-Man",
-    "Deadpool",
-    "Captain America: Civil War",
-    "X-Men: Apocalypse",
-    "Doctor Strange",
-    "Logan",
-    "Guardians of the Galaxy Vol. 2",
-    "Spider-Man: Homecoming",
-    "Thor: Ragnarok",
-    "Black Panther"];
+  "X-Men",
+  "Blade II",
+  "Spider-Man",
+  "Daredevil",
+  "Hulk",
+  "Spider-Man 2",
+  "Blade: Trinity",
+  "Elektra",
+  "Man-Thing",
+  "Fantastic Four",
+  "X-Men: The Last Stand",
+  "Ghost Rider",
+  "Spider-Man 3",
+  "Fantastic Four: Rise of the Silver Surfer",
+  "Iron Man",
+  "The Incredible Hulk",
+  "Punisher: War Zone",
+  "X-Men Origins",
+  "Iron Man 2",
+  "Thor",
+  "X-Men: First Class",
+  "Captain America: The First Avenger",
+  "Ghost Rider: Spirit of Vengeance",
+  "Avengers",
+  "The Amazing Spider-Man",
+  "Iron Man 3",
+  "The Wolverine",
+  "Thor: The Dark World",
+  "Captain America: The Winter Soldier",
+  "The Amazing Spider-Man 2",
+  "X-Men: Days of Future Past",
+  "Guardians of the Galaxy",
+  "Big Hero 6",
+  "Avengers: Age of Ultron",
+  "Ant-Man",
+  "Deadpool",
+  "Captain America: Civil War",
+  "X-Men: Apocalypse",
+  "Doctor Strange",
+  "Logan",
+  "Guardians of the Galaxy Vol. 2",
+  "Spider-Man: Homecoming",
+  "Thor: Ragnarok",
+  "Black Panther"];
+
+var char_array = [];
+
+function displayMovieInfo() {
+
+  for (var i = 0; i < movies.length; i++) {
+
+    var movie = movies[i];
+    var queryURL = "https://www.omdbapi.com/?t=" + movie + "&y=&plot=short&apikey=trilogy";
+    // var queryURL1 = "https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=2&q=CaptainAmericatrailer&key=AIzaSyC8th4wDxjLmTn1fONnkSMaUaGAGTUNQRA"
+
+    fetch(queryURL).then(function (response) {
+      return (response.json());
+    }).then(function (response) {
+      console.log("****THE VANILLA WAY****");
+      // console.log(response);
+      createButton(response);
+
+    }).catch(function (response) {
+      console.log("***** This failed *****")
+      console.log(response);
+    });
+
+    localStorage.setItem("movie" + i, movies[i]);
+    localStorage.setItem("movies", JSON.stringify(movies));
+  }
+
+}//displayMovieInfo
 
 
-    function displayMovieInfo() {
+function createButton(response) {
+  var movieDiv = $("<div class='movie'>");
+  var a = $("<button id = 'movie_id'>");
+  a.addClass("movie-btn");
+  a.attr("data-name", response.imdbID);
+  a.text(response.Title);
+  movieDiv.append(a)
 
-      for (var i = 0; i < movies.length; i++) {
-  
-        var movie = movies[i];
-        var queryURL = "https://www.omdbapi.com/?t=" + movie + "&y=&plot=short&apikey=trilogy";
-        // var queryURL1 = "https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=2&q=CaptainAmericatrailer&key=AIzaSyC8th4wDxjLmTn1fONnkSMaUaGAGTUNQRA"
-  
-        fetch(queryURL).then(function (response) {
-          return (response.json());
-        }).then(function (response) {
-          console.log("****THE VANILLA WAY****");
-          // console.log(response);
-          createButton(response);
-  
-        }).catch(function (response) {
-          console.log("***** This failed *****")
-          console.log(response);
-        });
-  
-        localStorage.setItem("movie" + i, movies[i]);
-        localStorage.setItem("movies", JSON.stringify(movies));
-      }
-  
-    }//displayMovieInfo
+  var released = response.Released;
+  var pTwo = $("<p>").text("Released: " + released);
 
+  movieDiv.append(pTwo);
 
-    function createButton(response) {
-      var movieDiv = $("<div class='movie'>");
-      var a = $("<button id = 'movie_id'>");
-      a.addClass("movie-btn");
-      a.attr("data-name", response.imdbID);
-      a.text(response.Title);
-      movieDiv.append(a)
-  
-      var released = response.Released;
-      var pTwo = $("<p>").text("Released: " + released);
-  
-      movieDiv.append(pTwo);
-  
-      var plot = response.Plot;
-      var pThree = $("<p>").text("Plot: " + plot);
-  
-      movieDiv.append(pThree);
-  
-      // ------------------For Youtube --------------------------//
-  
-      var play = $(' <div class="youtube-player" data-id="VIDEO_ID"></div>');
-      movieDiv.append(play);
-  
-      // ------------------For Youtube --------------------------//
-  
-      $(".container").prepend(movieDiv);
-    }//createButton
-  
-    displayMovieInfo();
-  
-    // using event bubbling to provide a single listener
-    document.querySelector('.container').addEventListener("click", function (e) {
-      console.log(e)
-      console.log(e.srcElement);
-  
-      if (e.srcElement.className == "movie-btn") {
-        console.log("Button ID :" + e.srcElement.id);
-        console.log("Movie Id :" + e.srcElement.dataset.name);
-        console.log("Class Name :" + e.srcElement.className);
-        let movie_id = e.srcElement.dataset.name;
-        console.log("passed ID " + e.srcElement.dataset.name);
-        character_array(movie_id);
-      }// movie-btn lisenter
-    });// container listener
-  
-    function character_array(movieID) {
-      var movie_ID = movieID
-      // console.log(movie_ID);
-      var queryURL2 = "https://api.themoviedb.org/3/movie/" + movie_ID + "/credits?api_key=cf7b01cd6ffa681f8ced55fe3eac526f";
-      console.log(queryURL2);
-      var char_array = [];
-      fetch(queryURL2).then(function (response) {
-        return (response.json());
-      }).then(function (response) {
-        console.log("****THE VANILLA WAY****");
-        console.log(response);
-        var special_char;
-        
-        //split the item into separate strings if it is separated by a speacial character
-        for (i = 0; i < 10; i++) {
-          if (response.cast[i].character.includes('/')){
-            special_char = '/';
-            var characters_split;
-            characters_split= response.cast[i].character.split(special_char); //store the split array
-            for(var j = 0;j<characters_split.length; j++){
-              char_array.push(characters_split[j]);  //push each item of the array into the char_array
-            }
-          }else if( response.cast[i].character.includes('(')){//find string that contains "("
+  var plot = response.Plot;
+  var pThree = $("<p>").text("Plot: " + plot);
+
+  movieDiv.append(pThree);
+
+  // ------------------For Youtube --------------------------//
+
+  var play = $(' <div class="youtube-player" data-id="VIDEO_ID"></div>');
+  movieDiv.append(play);
+
+  // ------------------For Youtube --------------------------//
+
+  $(".container").prepend(movieDiv);
+}//createButton
+
+displayMovieInfo();
+
+// using event bubbling to provide a single listener
+document.querySelector('.container').addEventListener("click", function (e) {
+  // console.log(e)
+  // console.log(e.srcElement);
+
+  if (e.srcElement.className == "movie-btn") {
+    // console.log("Button ID :" + e.srcElement.id);
+    // console.log("Movie Id :" + e.srcElement.dataset.name);
+    // console.log("Class Name :" + e.srcElement.className);
+    let movie_id = e.srcElement.dataset.name;
+    // console.log("passed ID " + e.srcElement.dataset.name);
+    character_array(movie_id);
+  }// movie-btn lisenter
+});// container listener
+
+function character_array(movieID) {
+  var movie_ID = movieID
+  // console.log(movie_ID);
+  var queryURL2 = "https://api.themoviedb.org/3/movie/" + movie_ID + "/credits?api_key=cf7b01cd6ffa681f8ced55fe3eac526f";
+  // console.log(queryURL2);
+
+  fetch(queryURL2).then(function (response) {
+    return (response.json());
+  }).then(function (response) {
+    // console.log("****THE VANILLA WAY****");
+    console.log(response);
+    var special_char;
+    // var temp_character;
+
+    //split the item into separate strings if it is separated by a speacial character
+    for (i = 0; i <= 10; i++) {//this for loop will create a first original array with name of characters 
+      if (response.cast[i].character.includes('/')) {
+        special_char = '/';
+        var characters_split;
+        characters_split = response.cast[i].character.split(special_char); //store the split array
+        for (var j = 0; j < characters_split.length; j++) {
+          //push each item of the array into the char_array
+          if (characters_split[j].includes('(')) {//find string that contains "("
             special_char = '(';
-            characters_split= response.cast[i].character.split(special_char); //split string into array
-            console.log(characters_split);
+            characters_split = characters_split[j].split(special_char); //split string into array
+            // console.log(characters_split);
             char_array.push(characters_split[0]);//only grab first string of array, the first string is always the name of character in movie
-          }else{
-            char_array.push(response.cast[i].character);
+          } else {
+            char_array.push(characters_split[j]);
           }
-          console.log(char_array);
-          console.log("Array of first 10 characters in movie :"+ char_array);
         }
-        // console.log(char_array);
-      }).catch(function (response) {
-        console.log("***** This failed *****")
-        console.log(response);
-      });
-    }//character Array 
-  
+
+
+      } else if (response.cast[i].character.includes('(')) {//find string that contains "("
+        special_char = '(';
+        characters_split = response.cast[i].character.split(special_char); //split string into array
+        // console.log(characters_split);
+        char_array.push(characters_split[0]);//only grab first string of array, the first string is always the name of character in movie
+      }
+      else {
+        char_array.push(response.cast[i].character);
+      }
+
+      // console.log(char_array);
+      // console.log("Array of first 10 characters in movie :"+ char_array);
+    }
+    console.log(char_array);
+  }).catch(function (response) {
+    console.log("***** This failed *****")
+    console.log(response);
+  });
+  console.log("Character array: " + char_array);
+  setTimeout(gen_character, 500);
+
+}//character Array 
+
+function gen_character() {
+
+  // ------------------IGNORE FOR NOW -----------------------//
+
+  for (var k = 0; k < char_array.length; k++) {
+    if (char_array[k].includes('') || char_array[k].includes('-') || char_array[k].includes('The')) {
+     
+      var temp_character = [char_array[k]];
+      console.log(temp_character);
+      var temp_char = temp_character.join('-');
+      console.log(temp_char);
+      // char_array.push(temp_char);
+      // temp_char =temp_character.join('');
+      // char_array.push(temp_char);
+    } else {
+      return (char_array);
+    }
+  }
+
+  // ------------------IGNORE FOR NOW -----------------------//
+}
+
 
 
 
@@ -257,14 +296,14 @@ function getQuery(search) {
 
 
 function getComics(charId) {
-// need to set a boolean here for if a user has chosen a new character.
-//  let newURL = marvURL + "characters/" + charId + "/comics?limit=100&offset=100" + apiAuth;
+  // need to set a boolean here for if a user has chosen a new character.
+  //  let newURL = marvURL + "characters/" + charId + "/comics?limit=100&offset=100" + apiAuth;
   let newURL = marvURL + "characters/" + charId + "/comics?limit=100&offset=100" + apiAuth;
   console.log("The comics fired");
   console.log(charId);
   console.log(newURL);
   loadMore(charId);
-    
+
 
   //https://gateway.marvel.com:443/v1/public/characters/1009624/comics?limit=100&apikey=0c59a3c44e014ddc1ffcdf3ff74fc90f
 
@@ -307,11 +346,11 @@ var listElm = document.querySelector('#infinite-comiclist');
 // Add 20 items.
 var nextItem = 0;
 //var loadMore = function () 
-function loadMore(char){
+function loadMore(char) {
 
   let charId = "1009610";
 
-  let newURL = marvURL + "characters/" + charId + "/comics?limit=10&offset=" +nextItem + apiAuth;
+  let newURL = marvURL + "characters/" + charId + "/comics?limit=10&offset=" + nextItem + apiAuth;
   const kevsServer = "https://mighty-river-19291.herokuapp.com/cors";
   var data = {
     url: newURL,
@@ -330,26 +369,26 @@ function loadMore(char){
     .then(function (response) {
       console.log("****THE Load more function****");
       console.log(response);
-   // console.log(Object.keys(response).length);
-    console.log(response.data.results.length);
-     // console.log(response.data.results[0].title);
+      // console.log(Object.keys(response).length);
+      console.log(response.data.results.length);
+      // console.log(response.data.results[0].title);
 
       // debuggin lines printing title to console.
-      for (var i = 0; i < response.data.results.length ; i++) {
+      for (var i = 0; i < response.data.results.length; i++) {
         console.log(response.data.results[i].title);
       }
-      
-      for (var i = 0; i < response.data.results.length ; i++) {
-         var item = document.createElement('li');
-         item.innerText = 'Title ' + response.data.results[i].title;
-         listElm.appendChild(item);
-         console.log("Comic Image: " +response.data.results[i].thumbnail.path + "." + response.data.results[i].thumbnail.extension);
-       }
 
-       nextItem += 11;
-       console.log("The Value of nextItem :" +  nextItem );    
+      for (var i = 0; i < response.data.results.length; i++) {
+        var item = document.createElement('li');
+        item.innerText = 'Title ' + response.data.results[i].title;
+        listElm.appendChild(item);
+        console.log("Comic Image: " + response.data.results[i].thumbnail.path + "." + response.data.results[i].thumbnail.extension);
+      }
+
+      nextItem += 11;
+      console.log("The Value of nextItem :" + nextItem);
     });
-    
+
 }
 
 // Detect when scrolled to bottom.
